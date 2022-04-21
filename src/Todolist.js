95dc5ae8e0325ce534c47982365d45ae838d64a8
ohjuhye <bbangbbang562@gmail.com>
2022-04-21 19:09:54 +0900

    기능 추가

--- src/Todolist.js
+++ src/Todolist.js
@@ -2,11 +2,33 @@ const input = document.getElementById('add_box');
 const button = document.getElementById('submit_img');
 const list= document.getElementById('add_list');
 
+
 const inputText = () => {
     // console.log(input.value);
-    const spans = document.createElement('div');
-    spans.innerHTML = input.value;
-    list.appendChild(spans);
+    const content = document.createElement('div');
+    content.id="content";
+
+    const span=document.createElement('span');
+    const input=document.createElement('input');
+    input.id="check";
+    input.type="checkbox";
+    
+    const label=document.createElement('label');
+    label.id="check";
+
+    const div=document.createElement('div');
+    div.id="add_text";
+
+    const div_img=document.createElement('div');
+    const edit_img=document.createElement('img');
+    edit_img.id="edit_img";
+
+    const delete_img=document.createElement('img');
+    delete_img.id="delete_img";
+
+
+    // spans.innerHTML = input.value;
+    list.appendChild(content);
   }
 
 button.addEventListener('click', inputText);
@@ -16,3 +38,4 @@ input.addEventListener('keydown', (event) => {
     inputText();
   }
 }); 
+
