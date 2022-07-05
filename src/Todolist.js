2d80f5920a221b08b39876e5097b26974d05d9c5
ohjuhye <bbangbbang562@gmail.com>
2022-07-05 22:26:56 +0900

    기능 수정

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,58 +1,59 @@
-const input_text = document.getElementById('add_box');
-const button = document.getElementById('submit_img');
-const list= document.getElementById('add_list');
+const input_text = document.getElementById("add_box");
+const button = document.getElementById("submit_img");
+const list = document.getElementById("add_list");
+let modify_btn = document.getElementById("modify_btn");
 
 let count = 0;
 
 const inputText = () => {
-    // console.log(input.value);
-    const content = document.createElement('div');
-    content.className="content";
-    content.id=`content${count}`;
+  const content = document.createElement("div");
+  content.className = "content";
+  content.id = `content${count}`;
 
-    content.innerHTML=`
+  content.innerHTML = `
+  <div id="list_id${count}">  
     <span>
       <input id="check${count}" class="check" type="checkbox" readonly>
       <label for="check${count}"></label>
-    <div id="add_text">${input_text.value}</div>
+      <div id="add_text${count}">${input_text.value}</div>
     </span>
-    <div>
-      <img id="delete_img" onclick="delete_button(${count})" src="/assets/img/cancel-button.png">
-    </div>`
- 
-    
-    
-    const hr=document.createElement('hr')
-    hr.id=`hr${count}`
-    
-    count++;
-    
-    list.appendChild(content);
-    list.appendChild(hr);
+    <img id="delete_img" onclick="delete_button(${count})" src="../assets/img/cancel-button.png" />
+    <input id="modify_input${count}" value="${input_text.value}" />
+    <button id="modify_btn" onclick="ModifyHandle(${count})">수정</button>
+  </div>`;
 
-  }
+  count++;
+  // <hr id="hr${count}"></hr>
 
-button.addEventListener('click', inputText);
+  list.appendChild(content);
+};
+
+button.addEventListener("click", inputText);
 
 function delete_text() {
   const currentVal = input_text.value;
-  input_text.value ='';
+  input_text.value = "";
 }
 
 button.onclick = delete_text;
 
-input_text.addEventListener('keydown', (event) => {
-  if (event.keyCode === 13) { 
+input_text.addEventListener("keydown", (event) => {
+  if (event.keyCode === 13) {
     inputText();
-    input_text.value ='';
+    input_text.value = "";
   }
-}); 
-
+});
 
 const delete_button = (id) => {
-  const node = list.querySelector(`#content${id}`);
-  const hr = list.querySelector(`#hr${id}`);
+  let node = document.getElementById(`list_id${id}`);
+  console.log(node);
 
-  node.remove()
-  hr.remove()
+  node.remove();
+};
+
+function ModifyHandle(id) {
+  let value = document.getElementById(`modify_input${id}`).value;
+  let defaultText = document.getElementById(`add_text${id}`);
+
+  defaultText.innerText = value;
 }
